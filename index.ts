4833dcee7ad7f4714f6868d6681078e9370f5b93
ashanfernando <ashan256@gmail.com>
2023-07-05 15:43:27 +0530

    Updating action

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import * as core from "@actions/core";
-const { context, getOctokit } = require('@actions/github');
 import { exec } from "@actions/exec";
 import run, { ExecFunction } from "./scripts/commit-bitmap";
 
 try {
   const wsDir: string = core.getInput("ws-dir") || process.env.WSDIR || "./";
+  const skipPush: string | undefined = core.getInput('skip-push');
   const stdExec: ExecFunction = (command: string, options?: {cwd: string}): Promise<number> => exec(command, [], options);
-  run(stdExec, wsDir);
+  run(stdExec, skipPush, wsDir);
 } catch (error) {
   core.setFailed((error as Error).message);
 }
--- scripts/commit-bitmap.ts
+++ scripts/commit-bitmap.ts
@@ -1,9 +1,12 @@
 export type ExecFunction = (command: string, options?: {cwd: string}) => Promise<number>;
 
-const run: (exec: ExecFunction, wsdir: string) => Promise<void> = async (exec, wsdir) => {
+const run: (exec: ExecFunction, skipPush: string, wsdir: string) => Promise<void> = async (exec, skipPush, wsdir) => {
   await exec('git add .bitmap', { cwd: wsdir });
   await exec('git commit -m "update .bitmap with new component versions (automated). [skip-ci]"', { cwd: wsdir });
-  await exec('git push', { cwd: wsdir });
+ 
+  if(!skipPush){
+    await exec('git push', { cwd: wsdir });
+  }
 }
 
 export default run;
